1acba81a649a7eaef83a509fe27ba4c60a863e8a
zhengyongrui <zyr@outlook.com>
2019-12-01 00:29:52 +0800

    初始代码

--- microLearning/pages/login/login.vue
+++ microLearning/pages/login/login.vue
@@ -13,6 +13,11 @@
         <view class="btn-row">
             <button type="primary" class="primary" @tap="bindLogin">登录</button>
         </view>
+       <view class="action-row">
+            <navigator url="../reg/reg">注册账号</navigator>
+            <text>|</text>
+            <navigator url="../pwd/pwd">忘记密码</navigator>
+        </view>
         <view class="oauth-row" v-if="hasProvider" v-bind:style="{top: positionTop + 'px'}">
             <view class="oauth-image" v-for="provider in providerList" :key="provider.value">
                 <image :src="provider.image" @tap="oauth(provider.value)"></image>
@@ -75,6 +80,24 @@
                 this.positionTop = uni.getSystemInfoSync().windowHeight - 100;
             },
             bindLogin() {
+                /**
+                 * 客户端对账号信息进行一些必要的校验。
+                 * 实际开发中，根据业务需要进行处理，这里仅做示例。
+                 */
+                if (this.account.length < 5) {
+                    uni.showToast({
+                        icon: 'none',
+                        title: '账号最短为 5 个字符'
+                    });
+                    return;
+                }
+                if (this.password.length < 6) {
+                    uni.showToast({
+                        icon: 'none',
+                        title: '密码最短为 6 个字符'
+                    });
+                    return;
+                }
                 /**
                  * 下面简单模拟下服务端的处理
                  * 检测用户账号密码是否在已注册的用户列表中
